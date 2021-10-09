c77b17837f875e5d40d12015555122f8704c1862
aakarshank <69320187+aakarshank@users.noreply.github.com>
2021-10-09 14:03:48 -0500

    Create talkToComputer.py
    simple program for user interaction with the computer!

---
+++ talkToComputer.py
@@ -0,0 +1,4 @@
+#input takes user input
+name = input("What is your name, user?")
+location = input("Where are you from "+name+"?")
+print("Nice to meet you," + name+ ". I will come to visit you in " + location+". Until then, take care!")
