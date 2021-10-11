3a93e051f1e607a969325267b940a3be3834dc3f
aakarshank <69320187+aakarshank@users.noreply.github.com>
2021-10-11 12:55:47 -0500

    Update talk_to_computer.py

--- talk_to_computer.py
+++ talk_to_computer.py
@@ -1,5 +1,5 @@
 #input takes user input
 name = input("What is your name, user?")
-location = input("Where are you from "+name+"?")
+location = input("Where are you from, "+name+"?")
 print("Nice to meet you, "+name)
 print("I will come to visit you in "+location)
