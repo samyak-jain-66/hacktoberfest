d5732ff97b6362e9655bd44345afbaa6241e8b20
nohli <43643339+nohli@users.noreply.github.com>
2021-10-12 08:48:41 +0200

    Move file to contributions folder, auto-format and use string interpolation

---
+++ contributions/talk_to_computer.py
@@ -0,0 +1,5 @@
+# input takes user input
+name = input(f"What is your name, user? ")
+location = input(f"Where are you from, {name}? ")
+print(f"Nice to meet you, {name}.")
+print(f"I will come to visit you in {location}.")
--- talk_to_computer.py
+++
@@ -1,5 +0,0 @@
-#input takes user input
-name = input("What is your name, user?")
-location = input("Where are you from, "+name+"?")
-print("Nice to meet you, "+name)
-print("I will come to visit you in "+location)
